66df96b548e359f1824b786a81d10f1e62d24b2f
Kanakana0529 <92347140+Kanakana0529@users.noreply.github.com>
2023-10-17 12:10:38 +0900

    change3-1

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -1,16 +1,27 @@
 // テキスト「リサイズするプログラムを作る」
-function setup(){
-  createCanvas(windowWidth, windowHeight);
-}
+  function setup(){
+   createCanvas(windowWidth, windowHeight);
+ }
+ 
+ function windowResized(){
+   resizeCanvas(windowWidth, windowHeight);
+ }
+  let size=50
+  let x=0
+  let y=100
+  let z=1
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-function draw(){
-  background(160, 192, 255);
-  for(let i = 0; i < 10; i++){
-    ellipse(width * i / 10, height * (10 - i) / 10, 20);
-  }
-  text("center!", width * 0.5, height * 0.5);
-}
+ function draw(){
+   background(160, 192, 255);
+   x=(x+z)% y;
+     if (x<y/2){
+    size=x+50 ;
+   }else{
+    size=(y-x)+50;
+   }
+   for(let i = 0; i < 10; i++){
+     ellipse(width * i / 10, height * (10 - i) / 10, 20);
+   }
+   text("center!", width * 0.5, height * 0.5);
+ }
+ 
