1a447a1ab3b3e5edef327da12dab38f8781cd03b
Kanakana0529 <92347140+Kanakana0529@users.noreply.github.com>
2023-10-24 11:45:08 +0900

    section3-q1 change

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -5,24 +5,24 @@
  
  function windowResized(){
    resizeCanvas(windowWidth, windowHeight);
- }
-  let size=1
-  let x=0
-  let y=100
-  let z=1
+ }       //ここまでは雛形やったと思います
+  let size=50
+  let count=0
+  let cycle=100
+  let increment=1
 
  function draw(){
    background(160, 192, 255);
-   x=(x+z)% y;
+   count=(count+increment)% cycle;
    if(keyIsPressed){
-    z=2;
+    increment=2;
    } else{
-    z=1;
+    increment=1;
    }
-     if (x<y/2){
-    size=x+50 ;
+     if (count<cycle/2){
+    size=count+50 ;
    }else{
-    size=(y-x)+50;
+    size=(cycle-count)+50;
    }
    for(let i = 0; i < 10; i++){
      ellipse(width * i / 10, height * (10 - i) / 10, size);
