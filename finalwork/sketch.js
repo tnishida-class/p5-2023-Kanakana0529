c47eac1a760cd948921134da1be9ac78f55ad788
Kanakana0529 <92347140+Kanakana0529@users.noreply.github.com>
2023-11-27 14:24:08 +0900

    change finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,4 +1,5 @@
 let balls = [];
+let snowpiles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,6 +7,92 @@ function setup() {
 
 function draw() {
   background(160, 192, 255);
+
+  if (frameCount % 20 === 0) {
+    const b = {
+      x: random(width),
+      y: 0,
+      size: random(10, 50),
+      vx: random(-2, 2),
+      vy: random(0, 3)
+    };
+    balls.push(b);
+  }
+
+  // 雪の描画と移動
+  for (let i = 0; i < balls.length; i++) {
+    let b1 = balls[i];
+    noStroke();
+    fill(255);
+    ellipse(b1.x, b1.y, b1.size);
+    b1.x += b1.vx;
+    b1.y += b1.vy;
+
+    if (b1.y > height - 10) {
+      // ボールを地面の高さに補正
+      b1.y = height - 10;
+
+      let snowPile = {
+        x: b1.x,
+        y: b1.y,
+        size: b1.size
+      };
+      snowpiles.push(snowPile);
+
+      // ボールを削除
+      balls.splice(i, 1);
+      i--;
+    } else if (b1.x < 0 || b1.x > width || b1.y < 0 || b1.y > height) {
+      // ボールが画面外に出たら削除
+      balls.splice(i, 1);
+      i--;
+    }
+  }
+
+  // 雪の積もりを描画
+  fill(255); // 雪の色
+  for (let i = 0; i < snowpiles.length; i++) {
+    let pile = snowpiles[i];
+    ellipse(pile.x, pile.y, pile.size);
+  }
+
+  {
+  fill(255);
+  noStroke();
+  ellipse(width*6/8,height*6.75/10,width/8,height*2/10);
+  //の中心,中心,半径,半径
+  ellipse(width*6/8,height*5/10,width/9,height*2/10);
+  //円の中心,中心,半径,半径
+
+//雪だるまの帽子△ver
+//fill(255,0,0);
+//noStroke();
+//beginShape();
+//vertex(width*6/8,height*3/10);
+//vertex(width*6.4/8,height*4/10);
+//vertex(width*5.6/8,height*4/10);
+//endShape(CLOSE);
+
+//雪だるまの帽子□ver
+fill(255,0,0);
+noStroke();
+rect(width*5.85/8,height*3/10,width*1.5/40,height/10)
+
+//雪だるまの顔の中身
+fill(0);
+  noStroke(0);
+  ellipse(width*23/32,height*9/20,10,10);
+  ellipse(width*24/32,height*9/20,10,10);
+
+  fill(251,177,97);
+  noStroke();
+  ellipse(width*47/64,height*19/40,15,15);
+
+  fill(255);
+  stroke(0)
+  arc(width*47/64,height*10/20,40,20,0,PI);
+
+}
 
   fill(139, 69, 19); // 茶色
   rect(180, 400, 40, 100);
@@ -22,38 +109,12 @@ function draw() {
   vertex(150, 300); // 左上
   endShape(CLOSE);
 
-  // 雪の生成
-  if (frameCount % 20 === 0) {
-    const b = {
-      x: random(width),
-      y: 0,
-      size: random(10, 50),
-      vx: random(-2, 2),
-      vy: random(0, 4)
-    };
-    balls.push(b);
-  }
-
-  // 雪の描画と移動
-  for (let i = 0; i < balls.length; i++) {
-    let b1 = balls[i];
-    noStroke();
-    fill(255)
-    ellipse(b1.x, b1.y, b1.size);
-    b1.x += b1.vx;
-    b1.y += b1.vy;
-
-    // ボールが画面外に出たら削除
-    if (b1.x < 0 || b1.x > width || b1.y < 0 || b1.y > height) {
-      balls.splice(i, 1);
-      i--;
-    }
-  }
-
   
   
-
 }
+  
+
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
